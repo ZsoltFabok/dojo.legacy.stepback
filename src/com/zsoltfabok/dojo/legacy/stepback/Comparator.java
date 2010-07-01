36bec638dd2eed837ce1797c09a82f3f1d804b7e
Zsolt Fabok <me@zsoltfabok.com>
2010-07-02 01:23:10 +0200

    Version demonstrated on video:   Add test case for safe refactoring   Perform refactoring   Add new functionality

--- src/com/zsoltfabok/dojo/legacy/stepback/Comparator.java
+++ src/com/zsoltfabok/dojo/legacy/stepback/Comparator.java
@@ -1,29 +1,14 @@
 package com.zsoltfabok.dojo.legacy.stepback;
+
+
 public class Comparator {
     public boolean same(String string) {
-        char[] data = string.toCharArray();
-        char[] first = null;
-        char[] second = null;
-        boolean value = false;
+        String[] words = string.split(" ");
 
-        for (int i = 0; i < data.length; i++)
-        if (data[i] == ' ') {
-            first = new char[i];
-            second = new char[data.length - i - 1];
-            System.arraycopy(data, 0, first, 0, i);
-            System.arraycopy(data, i + 1, second, 0, data.length - i - 1);
-            if (first.length == second.length)
-                for (int j = 0; j < first.length; j++)
-                    if (first[j] == second[j])
-                        value = true;
-                    else {
-                        value = false;
-                        break;
-                    }
-            else
-                return false;
-            break;
+        if (words.length == 2) {
+            return words[0].equalsIgnoreCase(words[1]);
+        } else {
+            return false;
         }
-        return value;
     }
 }
--- test/com/zsoltfabok/dojo/legacy/stepback/ComparatorTest.java
+++ test/com/zsoltfabok/dojo/legacy/stepback/ComparatorTest.java
@@ -1,5 +1,41 @@
 package com.zsoltfabok.dojo.legacy.stepback;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class ComparatorTest {
+    private Comparator comparator = new Comparator();
+
+    @Test
+    public void shouldReturnTrueForTheSameWords()  {
+        assertTrue(new Comparator().same("word word"));
+    }
+
+    @Test
+    public void shouldReturnFalseForDifferentWords()  {
+        assertFalse(comparator.same("ward word"));
+    }
+
+    @Test
+    public void shouldReturnFalseForWordsWithDifferentLength()  {
+        assertFalse(comparator.same("worda word"));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheFirstWordIsMissing()  {
+        assertFalse(comparator.same(" word"));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheSecondWordIsMissing()  {
+        assertFalse(comparator.same("word "));
+    }
+
+    @Test
+    public void shouldReturnTrueForTheSameButNonCaseSensitiveWords()  {
+        assertTrue(new Comparator().same("wORd word"));
+    }
 
 }
